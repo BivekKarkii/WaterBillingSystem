dda5f3b7a299b0d7e1b062ac097e4ec9d6dd7e42
BivekKarkii <np03cs4a210046@heraldcollege.edu.np>
2023-04-08 14:17:30 +0545

    consumer view from db

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from home import views
-from home.views import homeview, signupview, registerview, login_request, welcomeview, \
-    forgetPassword, changePassword, all_user, logOut, aboutview, supportview, contactview
+from home.views import *
 from newWaterBillingSystem import settings
 
 app_name = "home"
@@ -13,13 +12,20 @@ urlpatterns = [
     path('', homeview, name='homepage'),
     path('login', login_request, name='login-page'),
     path('register', registerview, name="register"),
+
+    path('edit', editView, name='edit'),
+    path('update/<str:id>', updateView, name='update'),
+    path('delete/<str:id>', deleteView, name='delete'),
+
     path('welcome', welcomeview, name="welcome"),
     path('forgetpassword', forgetPassword, name="forget-password"),
     path('changepassword/<token>/', changePassword, name="change-password"),
     path('logoout', logOut, name="logout"),
-    path('alluser', all_user),
+    path('alluser', consumerView),
     path('about', aboutview, name="about"),
     path('support', supportview, name="support"),
     path('contact', contactview, name="contact"),
+
+
 
 ]
